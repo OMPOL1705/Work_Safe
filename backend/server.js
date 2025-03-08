40e3853167e4ba3207ab2b4a6fd56dd36c5242a9
Om <ompol1705@gmail.com>
2025-03-08 12:35:41 +0530

    fff

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
